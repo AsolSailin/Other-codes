be025ae5b0bdb3639669a2180393ee5d08818812
Ruslana <garaeva-r_23@mail.ru>
2022-09-30 20:59:42 +0300

    Add code

--- Gauss/Gauss/Program.cs
+++ Gauss/Gauss/Program.cs
@@ -11,20 +11,29 @@ namespace NumericalMethods
         static void Main(string[] args)
         {
             int n = 2;
-            double x;
 
-            double[,] matrix = 
+            double[,] a =
             {
                 { 8.39, -8.99, -1.29, 8.86 },
                 { -1.05, 1.89, 4.91, 4.20 },
                 { -6.38, 8.36, 0.48, -5.06 }
             };
 
+            double[,] matrix = Calculate(n, a);
+
+            for (int i = 0; i <= n; i++)
+            {
+                Console.WriteLine(matrix[i, n + 1]);
+            }
+        }
+
+        static double[,] Calculate(int n, double[,] matrix)
+        {
             for (int i = 0; i <= n; i++)
             {
                 for (int j = n + 1; j >= i; j--)
                 {
-                    matrix[i, j] = matrix[i, j] / matrix[i,i];
+                    matrix[i, j] = matrix[i, j] / matrix[i, i];
                 }
 
                 for (int k = 0; k <= n; k++)
@@ -38,12 +47,7 @@ namespace NumericalMethods
                     }
                 }
             }
-
-            for (int i = 0; i <= n; i++)
-            {
-                x = matrix[i, n + 1];
-                Console.WriteLine(x);
-            }
+            return matrix;
         }
     }
 }

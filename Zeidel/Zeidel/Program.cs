1847d6a16a26a0d731eb338d0bde315a4a1ffae4
Ruslana <garaeva-r_23@mail.ru>
2022-10-08 00:20:04 +0300

    Add code

---
+++ Zeidel/Zeidel/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericalMethods
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            double e = 0.001;
+            double[,] a =
+            {
+                { 16.63, -0.24, -6.10, 7.29 },
+                { -3.45, -23.13, 1.11, -3.41 },
+                { 3.76, -8.72, -27.01, -8.19 },
+            };
+
+            double[] x = new double[n];
+            x = Colculate(n, e, a, x);
+
+            if (x.Length != 0)
+            {
+                for (int i = 0; i < x.Length; i++)
+                {
+                    Console.WriteLine(x[i] + " ");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Условие сходисмости не выполняется");
+            }
+        }
+
+        static double[] Colculate(int n, double e, double[,] matrix, double[] x)
+        {
+            double[] y = new double[n];
+            double d = 0;
+
+            d = GetConvergence(n, matrix, d); 
+
+            if (Math.Sqrt(d) < 1)
+            {
+                d = 2 * e;
+
+                while(d > e)
+                {
+                    d = 0;
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        y[i] = matrix[i, n];
+                        
+                        for (int j = 0; j < n; j++)
+                        {
+                            y[i] += matrix[i, j] * x[j];
+                        }
+
+                        d += Math.Pow((y[i] - x[i]), 2);
+                        x[i] = y[i];
+                    }
+
+                    d = Math.Sqrt(d);
+                }
+            }
+
+            return x;
+        }
+
+        static double GetConvergence(int n, double[,] matrix, double d)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i != j)
+                    {
+                        matrix[i, j] = -matrix[i, j] / matrix[i, i];
+                        d += Math.Pow(matrix[i, j], 2);
+                    }
+                }
+                matrix[i, n] = matrix[i, n] / matrix[i, i];
+                matrix[i, i] = 0;
+            }
+            return d;
+        }
+    }
+}

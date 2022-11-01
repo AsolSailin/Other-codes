22bc3448e15aad735bd1cc66ec84ac79637757d6
Ruslana <garaeva-r_23@mail.ru>
2022-11-01 09:27:05 +0300

    Add code

--- Lagrange/Lagrange/Program.cs
+++ Lagrange/Lagrange/Program.cs
@@ -10,13 +10,13 @@ namespace NumericalMethods
     {
         static void Main(string[] args)
         {
-            int n = 5;
+            int n = 3;
             // a) 1.02
             // б) 0.65
             // в) 1.28
-            double pointX = 1.02;
-            double[] x = new double[5] { 0.27, 0.93, 1.46, 2.11, 2.87 };
-            double[] y = new double[5] { 2.60, 2.43, 2.06, 0.25, -2.60 };
+            double pointX = 2.5;
+            double[] x = new double[3] { 0, 2, 3 };
+            double[] y = new double[3] { 1, 2, 1 };
             Console.WriteLine($"Ответ: {Calculate(n, pointX, x, y)}");
         }
 
--- NumericalIntegration/NumericalIntegration/Program.cs
+++ NumericalIntegration/NumericalIntegration/Program.cs
@@ -15,18 +15,18 @@ namespace NumericalMethods
             double e = 0.001;
             Func<double, double> func = x => x / (1 + Math.Pow(x, 3));
 
-            Console.WriteLine($"Ответ: {LeftCalculate(a, b, e, func)}");
-            Console.WriteLine($"Ответ: {RightCalculate(a, b, e, func)}");
-            /*Console.WriteLine($"Ответ: {Calculate(a, b, e, func)}");
-            Console.WriteLine($"Ответ: {Calculate(a, b, e, func)}");
-            Console.WriteLine($"Ответ: {Calculate(a, b, e, func)}");*/
+            Console.WriteLine($"Левый прямоугольник: {LeftRectangle(a, b, e, func)}");
+            Console.WriteLine($"Средний прямоугольник: {RightRectangle(a, b, e, func)}");
+            Console.WriteLine($"Правый прямоугольник: {MiddleRectangle(a, b, e, func)}");
+            Console.WriteLine($"Трапеция: {Trapezoid(a, b, e, func)}");
+            Console.WriteLine($"Симпсон: {Simpson(a, b, e, func)}");
         }
 
-        static double LeftCalculate(double a, double b, double e, Func<double, double> func)
+        public static double LeftRectangle(double a, double b, double e, Func<double, double> func)
         {
             double i0 = 0;
             double i1 = double.PositiveInfinity;
-            int n = 5;
+            int n = 50;
             double h, x;
 
             while ((i1 - i0) > e)
@@ -48,11 +48,11 @@ namespace NumericalMethods
             return i1;
         }
 
-        static double RightCalculate(double a, double b, double e, Func<double, double> func)
+        public static double RightRectangle(double a, double b, double e, Func<double, double> func)
         {
             double i0 = 0;
             double i1 = double.PositiveInfinity;
-            int n = 5;
+            int n = 50;
             double h, x;
 
             while ((i1 - i0) > e)
@@ -64,7 +64,7 @@ namespace NumericalMethods
 
                 for (int i = 0; i < n; i++)
                 {
-                    x = a + (i = 1) * h;
+                    x = a + (i + 1) * h;
                     i1 += func(x);
                 }
 
@@ -74,11 +74,11 @@ namespace NumericalMethods
             return i1;
         }
         
-        static double MiddleCalculate(double a, double b, double e, Func<double, double> func)
+        public static double MiddleRectangle(double a, double b, double e, Func<double, double> func)
         {
             double i0 = 0;
             double i1 = double.PositiveInfinity;
-            int n = 5;
+            int n = 50;
             double h, x;
 
             while ((i1 - i0) > e)
@@ -95,6 +95,61 @@ namespace NumericalMethods
                 }
 
                 i1 *= h;
+            }
+
+            return i1;
+        }
+
+        public static double Trapezoid(double a, double b, double e, Func<double, double> func)
+        {
+            double i0 = 0;
+            double i1 = double.PositiveInfinity;
+            int n = 50;
+            double h, x;
+
+            while ((i1 - i0) > e)
+            {
+                n *= 2;
+                i0 = i1;
+                h = (b - a) / n;
+                i1 = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    x = a + i * h;
+                    i1 += func(x);
+                }
+
+                i1 += (func(a) + func(a + (n - 1) * h)) / 2;
+                i1 *= h;
+            }
+
+            return i1;
+        }
+
+        public static double Simpson(double a, double b, double e, Func<double, double> func)
+        {
+            double i0 = 0;
+            double i1 = double.PositiveInfinity;
+            int n = 49;
+            double h, x1, x2;
+
+            while ((i1 - i0) > e)
+            {
+                n *= 2;
+                i0 = i1;
+                h = (b - a) / n;
+                i1 = 0;
+
+                for (int i = 0; i < n / 2; i++)
+                {
+                    x1 = a + (2 * i - 1) * h;
+                    x2 = a + 2 * i * h;
+                    i1 += 2 * func(x1) + func(x2);
+                }
+
+                i1 += (func(a) + func(a + (n - 1) * h)) / 2;
+                i1 *= h * 2 / 3;
             }
 
             return i1;

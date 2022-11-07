59125daf072cee29494c684cf7d5c9bbdb45ded8
Ruslana <garaeva-r_23@mail.ru>
2022-11-07 09:42:54 +0300

    Add code

--- Quadrature_Formula_Of_Gauss/Quadrature_Formula_Of_Gauss/Program.cs
+++ Quadrature_Formula_Of_Gauss/Quadrature_Formula_Of_Gauss/Program.cs
@@ -10,24 +10,17 @@ namespace NumericalMethods
     {
         static void Main(string[] args)
         {
-            // double a = 1, b = 2, e = 0.001, 
-            //     i0 = 0, i1 = double.PositiveInfinity, i2 = 0, 
-            //     h = 0, x = 0, x1 = 0, x2 = 0, result = 0;
-            // int n = 20;
-            // Func<double, double> func = x => 1 / (Math.Pow(x,6) * (1 + Math.Pow(x, 2)));
-            
-            
             double a = -1, b = 1, e = 0.001, 
                 i0 = 0, i1 = double.PositiveInfinity, i2 = 0, 
                 h = 0, x = 0, x1 = 0, x2 = 0, result = 0;
             int n = 16;
             Func<double, double> func = x => Math.Pow(2.72, Math.Pow(x, 2));
 
-            Console.WriteLine($"Левый прямоугольник: {LeftRectangle(a, b, e, n, func, i0, i1, i2, h, x, x1, x2, result)}");
+            Console.WriteLine($"Левый прямоугольник: {LeftRectangle(a, b, e, n, func, i0, i1, i2, h, x, result)}");
         }
 
         public static double LeftRectangle(double a, double b, double e, int n, Func<double, double> func, 
-            double i0, double i1, double i2, double h, double x, double x1, double x2, double result)
+            double i0, double i1, double i2, double h, double x, double result)
         {
             while ((i1 - i0) > e)
             {
@@ -38,10 +31,8 @@ namespace NumericalMethods
                 for (int i = 0; i < n; i++)
                 {
                     x = a + i * h;
-                    x1 = x + (h / 2) - (h / (2 * Math.Sqrt(3)));
-                    x2 = x + (h / 2) + (h / (2 * Math.Sqrt(3)));
-                    i1 += func(x1);
-                    i2 += func(x2);
+                    i1 += func(x + (h / 2) - (h / (2 * Math.Sqrt(3))));
+                    i2 += func(x + (h / 2) + (h / (2 * Math.Sqrt(3))));
                 }
 
                 result = i1 + i2;

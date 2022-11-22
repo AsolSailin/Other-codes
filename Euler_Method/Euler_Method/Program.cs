9f9ee5a2707ff3ee6614949a9cb85251c9928840
Ruslana <garaeva-r_23@mail.ru>
2022-11-22 23:13:55 +0300

    Add code

---
+++ Euler_Method/Euler_Method/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericalMethods
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 1, y = 2, h = 0.1, b = 2;
+            Func<double, double, double> func = (x, y) => x + y / x;
+
+            while (x <= b)
+            {
+                y += h * func(x, y);
+                x += h;
+                Console.WriteLine(y);
+            }
+        }
+    }
+}
